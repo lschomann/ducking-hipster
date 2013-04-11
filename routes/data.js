ec370598da3139361b05a3c8b982a72263abebf6
kurvenschubser <kurvenschubser@gmail.com>
2013-04-11 20:25:01 +0200

    add some crud method signatures + some preliminary implementations

--- routes/data.js
+++ routes/data.js
@@ -1,10 +1,3 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: malte
- * Date: 07.04.13
- * Time: 14:51
- * To change this template use File | Settings | File Templates.
- */
 
 /*
  * All routes associated with accessing or manipulating data in the database should go in here.
@@ -12,19 +5,77 @@
  *
  */
 
-/*
- * GET all rooms.
- */
 
+DataModel = require("datamodel/datamodel");
+
+// monkey patch Backbone.js to use postgres data store.
+Backbone = require("backbone-postgres");
+
+Passport = require("passport");
 
 
 module.exports = function(app){
 
+    /******************
+     *** BEGIN Room ***/
+
     /*
-     * GET all rooms as objects.
+     * GET all Room objects.
      */
 
-    app.get('/rest/getRooms', function(req, res){
+    app.get('/rest/room/all', function(req, res){
         res.json({});
     });
+
+    /*** END Room ***
+     ****************/
+
+    /********************
+     *** BEGIN Adress ***/
+
+    /*
+     * GET All Adress objects.
+     */
+
+    app.get('/rest/adress/', function(req, res){
+        res.json({});
+    });
+
+    /*
+     * GET The Adress object that has the given id. Returns 404 Not Found if the id does not exist.
+     */
+    app.get('/rest/adress/(\d+)', function(req, res){
+        var obj = DataModel.Adress.read({id: parseInt(req.body.params[0])});
+        return res.json(obj.toJSON());
+    });
+
+    /*
+     * POST Create Adress object with the given values. Return newly created object.
+     */
+    app.post('/rest/adress/', function(req, res){
+        var obj = new DataModel.Adress({"street": req.body.street, "number": req.body.number,
+                                        "city": req.body.city, "zip": req.body.zip});
+        return obj.save();
+    });
+
+    /*
+     * UPDATE Adress object with the given id.
+     */
+    app.put('/rest/adress/(\d+)', function(req, res){
+        var obj = DataModel.Adress.read({id: parseInt(req.body.params[0])});
+        return obj.save({"street": req.body.street, "number": req.body.number,
+                         "city": req.body.city, "zip": req.body.zip});
+
+    });
+
+    /*
+     * DELETE Adress object with the given id.
+     */
+    app.delete('/rest/adress/(\d+)', function(reg, res){
+        var obj = DataModel.Adress.read({id:parseInt(reg.body.params[0])});
+        return obj.delete();
+    });
+
+    /*** END Adress ***
+     ******************/
 };
