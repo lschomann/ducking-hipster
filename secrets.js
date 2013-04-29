971513d93a53c7429954783760678e1e0d75148e
kurvenschubser <kurvenschubser@gmail.com>
2013-04-29 21:22:14 +0200

    fix appfog.com VCAP_SERVICES postgresql config lookups.

--- secrets.js
+++ secrets.js
@@ -6,15 +6,26 @@
  * > Sentence Temporaryly suspended due to development status :D
  */
 
-
-var db_ = {
-    type: "postgres",
-    user: process.env.VCAP_SERVICES["postgresql-9.1"][0]["credentials"]["user"] || "postgres",
-    pass: process.env.VCAP_SERVICES["postgresql-9.1"][0]["credentials"]["password"] || "uiaenrtd",
-    host: process.env.VCAP_SERVICES["postgresql-9.1"][0]["credentials"]["host"] || "localhost",
-    port: process.env.VCAP_SERVICES["postgresql-9.1"][0]["credentials"]["port"] || "",               // leave as empty string for default port
-    database: process.env.VCAP_SERVICES["postgresql-9.1"][0]["credentials"]["name"] || "postgres"
-};
+var db_;
+if (process.env.VCAP_SERVICES){
+    db_= {
+        type: "postgres",
+        user: process.env.VCAP_SERVICES["postgresql-9.1"][0]["credentials"]["user"],
+        pass: process.env.VCAP_SERVICES["postgresql-9.1"][0]["credentials"]["password"],
+        host: process.env.VCAP_SERVICES["postgresql-9.1"][0]["credentials"]["host"],
+        port: process.env.VCAP_SERVICES["postgresql-9.1"][0]["credentials"]["port"],               // leave as empty string for default port
+        database: process.env.VCAP_SERVICES["postgresql-9.1"][0]["credentials"]["name"]
+    };
+} else {
+    var db_ = {
+        type: "postgres",
+        user: "postgres",
+        pass: "uiaenrtd",
+        host: "localhost",
+        port: "",               // leave as empty string for default port
+        database:  "postgres"
+    };
+}
 
 var getDBConnectionString_ = function(){
     return db_.type + "://" + db_.user + ":" + db_.pass + "@" + db_.host + "/" + db_.database
